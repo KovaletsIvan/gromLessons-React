430c906e8481cc5e1b0af5baa79c4609cc803f79
Ivan Kovalets <55809354+KovaletsIvan@users.noreply.github.com>
2021-08-30 23:02:57 +0300

    commit

--- react-1-3/webpack.config.js
+++ react-1-3/webpack.config.js
@@ -1,6 +1,12 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'review_build'),
+  },
   module: {
     rules: [
       {
