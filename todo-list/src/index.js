7c829d64688e4e0322fd7d5cfef2f39079e47012
Ivan Kovalets <55809354+KovaletsIvan@users.noreply.github.com>
2021-09-02 00:19:20 +0300

    commit

--- todo-list/src/index.js
+++ todo-list/src/index.js
@@ -4,6 +4,17 @@ import './index.css';
 
 const rootElem = document.querySelector('#root');
 
-const element = <h1 className="title">Todo List</h1>;
+const element = (
+  <>
+    <h1 className="title">Todo List</h1>
+    <main className="todo-list">
+      <div className="actions">
+        <input className="task-input" type="text" />
+        <button className="btn create-task-btn">Create</button>
+      </div>
+      <ul className="list"></ul>
+    </main>
+  </>
+);
 
 ReactDOM.render(element, rootElem);
