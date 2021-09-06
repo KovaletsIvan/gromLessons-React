99fd38a06ded14b6d241c9e00373ecba60c12b41
Ivan Kovalets <55809354+KovaletsIvan@users.noreply.github.com>
2021-09-06 22:52:18 +0300

    commit

--- react-3-5/src/Profile.jsx
+++ react-3-5/src/Profile.jsx
@@ -7,12 +7,12 @@ const date = (birthDate) => moment(birthDate).format("D MMM YY")
 const Profile = (props) => {
   return (
     <>
-      <span className="profile__name">
+      <div className="profile__name">
         {props.userData.firstName} {props.userData.lastName}
-      </span>
-      <span className="profile__birth">
+      </div>
+      <div className="profile__birth">
         Was born {date(props.userData.birthDate)} in {props.userData.birthPlace}
-      </span>
+      </div>
     </>
   )
 }
