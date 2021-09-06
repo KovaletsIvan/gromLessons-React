884a126baa8b8711b6bad718e9b41f94e4217898
Ivan Kovalets <55809354+KovaletsIvan@users.noreply.github.com>
2021-09-06 11:46:34 +0300

    commit

---
+++ react-3-3/src/index.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles.scss";
+import Comment from "./Comment.jsx"
+
+
+const rootElement = document.querySelector('#root');
+
+const userInfo = {
+  name: 'Tom',
+  avatarUrl:'https://cdn.icon-icons.com/icons2/2406/PNG/512/github_git_icon_145985.png'
+}
+
+ReactDOM.render(<Comment 
+user = {userInfo}
+text = 'Goog job!'
+date = {new Date()}
+/>, rootElement)
