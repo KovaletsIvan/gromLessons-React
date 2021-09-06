24f87b8ed72830c3a8fa1c10f19c2c004e05828a
Ivan Kovalets <55809354+KovaletsIvan@users.noreply.github.com>
2021-09-06 12:29:18 +0300

    commit

--- react-3-3/src/Avatar.jsx
+++ react-3-3/src/Avatar.jsx
@@ -2,10 +2,7 @@ import React from 'react';
 
 const Avatar = (props) => {
   return (
-    <>
-      <img className="avatar" src={props.user.avatarUrl} alt={props.user.name} />
-      <div className="user-info__name">{props.user.name}</div>
-    </>
+    <img className="avatar" src={props.avatarUrl} alt={props.name} />
   );
 };
 
--- react-3-3/src/Comment.jsx
+++ react-3-3/src/Comment.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import moment from 'moment';
-import UserInfo from './UserInfo.jsx'
+import UserInfo from './UserInfo'
 import "./comment.scss";
 
 
