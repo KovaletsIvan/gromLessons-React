f32d3fa617b23d2395d3f96793372a7aa752e8b5
Ivan Kovalets <55809354+KovaletsIvan@users.noreply.github.com>
2021-09-06 11:56:12 +0300

    commit

--- react-3-3/src/Avatar.jsx
+++ react-3-3/src/Avatar.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 
 const Avatar = (props) => {
   return (
-    <img className="avatar" src={props.user.avatarUrl} alt={props.user.name} />
+    <>
+      <img className="avatar" src={props.user.avatarUrl} alt={props.user.name} />
+      <div className="user-info__name">{props.user.name}</div>
+    </>
   );
 };
 
