c50fa268d0b141dde582cf334dd2a0717ee081b8
Ivan Kovalets <55809354+KovaletsIvan@users.noreply.github.com>
2021-09-07 20:57:11 +0300

    commit

--- react-3-3/src/index.jsx
+++ react-3-3/src/index.jsx
@@ -10,11 +10,14 @@ const userInfo = {
   name: 'Tom',
   avatarUrl: 'https://cdn.icon-icons.com/icons2/2406/PNG/512/github_git_icon_145985.png'
 }
+const author = {
+
+};
 
 
 ReactDOM.render(<Comment
   user={userInfo}
-  author = ''
+  author = {author}
   text='Goog job!'
   date= {new Date()}
 
