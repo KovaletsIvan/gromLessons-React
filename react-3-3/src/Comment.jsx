c848a02c421b984ebabfb5c520dfa3a5484dc284
Ivan Kovalets <55809354+KovaletsIvan@users.noreply.github.com>
2021-09-08 20:32:44 +0300

    commit

--- react-3-3/src/Comment.jsx
+++ react-3-3/src/Comment.jsx
@@ -10,7 +10,7 @@ const formatDate = (date) => moment(date).format("DD MMM YYYY");
 function Comment(props) {
   return (
     <div className="comment">
-      <UserInfo user={props.user} />
+      <UserInfo user={props.author} />
       <div className="comment__text" >{props.author.text}</div>
       <div className="comment__date" >{formatDate(props.author.date)}</div>
     </div>
