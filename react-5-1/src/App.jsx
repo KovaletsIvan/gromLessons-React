182cb9c3bc0344fe0afebdda29c86c0e9116ca17
Ivan Kovalets <55809354+KovaletsIvan@users.noreply.github.com>
2021-09-09 21:14:30 +0300

    commit

---
+++ react-5-1/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import GoodButton from './GoodButton';
+
+const App = () => {
+  return (
+    <>
+      <GoodButton />
+    </>
+  )
+}
+
+export default App;
---
+++ react-5-1/src/GoodButton.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const GoodButton = () => {
+  return <button className="fancy-button"
+  onClick = {()=>alert('Good job!')} >
+    Click mi!
+    </button>
+}
+
+
+export default GoodButton
