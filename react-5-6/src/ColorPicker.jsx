cf47bd2bf4d2dfcf2792d88af9ca2135697c4c94
Ivan Kovalets <55809354+KovaletsIvan@users.noreply.github.com>
2021-09-12 17:09:19 +0300

    commit

---
+++ react-5-6/src/ColorPicker.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+
+class ColorPicker extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      color: '',
+    };
+  }
+  toggleColors(e) {
+    const event = e.target.name;
+    this.setState({
+      color: event,
+    });
+
+  }
+  clear() {
+    this.setState({
+      color: ''
+    })
+  }
+  render() {
+    return (
+      <div>
+        <div className="picker__title">{this.state.color}</div>
+        <div>
+          <button
+            className="picker__button picker__button_coral"
+            name="Coral"
+            onMouseOver={this.toggleColors.bind(this)}
+            onMouseOut={this.clear.bind(this)}
+          ></button>
+          <button
+            className="picker__button picker__button_aqua"
+            name="Aqua"
+            onMouseOver={this.toggleColors.bind(this)}
+            onMouseOut={this.clear.bind(this)}
+          ></button>
+          <button
+            className="picker__button picker__button_bisque"
+            name="Bosquit"
+            onMouseOver={this.toggleColors.bind(this)}
+            onMouseOut={this.clear.bind(this)}
+          ></button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ColorPicker;
