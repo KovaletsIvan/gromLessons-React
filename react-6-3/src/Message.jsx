acb7c6cfb30510621fa5e8e1dba34dfc3f9ee164
Ivan Kovalets <55809354+KovaletsIvan@users.noreply.github.com>
2021-09-14 23:52:11 +0300

    commit

--- react-6-3/src/Message.jsx
+++ react-6-3/src/Message.jsx
@@ -3,23 +3,12 @@ import React from "react";
 const Message = ({ text }) => {
 
   return (
-    <div className="page">
+    <>
       {(!text) ? null : <div className="message">
         {text}
       </div>}
+    </>
 
-      <div className="actions">
-        <button className="btn">
-          Text 1
-        </button>
-        <button className="btn">
-          Text 2
-        </button>
-        <button className="btn">
-          Clear
-        </button>
-      </div>
-    </div>
   )
 }
 
--- react-6-3/src/Page.jsx
+++ react-6-3/src/Page.jsx
@@ -1,3 +1,44 @@
 import React from "react";
 import Message from "./Message";
 
+class Page extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      text: null,
+    }
+
+  }
+  setText = (vall) => {
+    this.setState({
+      text: vall
+    })
+  }
+  clear = () => {
+    this.setState({
+      text: null
+    })
+  }
+  render() {
+    return (
+      <div className="page">
+        <Message text={this.state.text} />
+
+        <div className="actions">
+          <button className="btn" onClick={() => this.setText('Hello, world!')}>
+            Text 1
+          </button>
+          <button className="btn" onClick={() => this.setText('Another exciting text.')}>
+            Text 2
+          </button>
+          <button className="btn" onClick={this.clear}>
+            Clear
+          </button>
+        </div>
+      </div>
+
+    )
+  }
+}
+
+export default Page
