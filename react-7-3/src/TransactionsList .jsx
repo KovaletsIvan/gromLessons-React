5eaa1ac6300ee07f420fc5603ba5daa11ea06060
Ivan Kovalets <55809354+KovaletsIvan@users.noreply.github.com>
2021-09-21 14:38:23 +0300

    commit

--- react-7-3/src/TransactionsList .jsx
+++ react-7-3/src/TransactionsList .jsx
@@ -2,20 +2,15 @@ import React from 'react';
 import Transaction from './Transaction';
 
 
-class TransactionsList extends React.Component {
-  constructor(props) {
-    super(props)
-  }
+const TransactionsList = (props) => {
 
-  render() {
-
-    return (
-      <ul className="transactions">
-        {this.props.transactions.slice().map(action => <Transaction key={action.id} {...action} />)}
-      </ul>
-    )
-  }
+  return (
+    <ul className="transactions">
+      {props.transactions.slice().map(action => <Transaction key={action.id} {...action} />)}
+    </ul>
+  )
 }
+
 
 
 export default TransactionsList
