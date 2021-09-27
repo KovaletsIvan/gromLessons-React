8821e3aa4071cb3b58d1b31ab1c00ddfbe06b828
Ivan Kovalets <55809354+KovaletsIvan@users.noreply.github.com>
2021-09-27 19:44:57 +0300

    commit

--- react-8-1/src/index.jsx
+++ react-8-1/src/index.jsx
@@ -1,13 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import Life from './Life'
 import './index.scss';
 
 
 const rootElement = document.querySelector('#root');
 
 
-
-
-
-ReactDOM.render(<App />, rootElement)
+ReactDOM.render(<Life/>, rootElement)
