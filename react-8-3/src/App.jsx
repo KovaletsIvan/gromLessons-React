ea943afeac150e185464639ffec4064ba2aeb6f8
Ivan Kovalets <55809354+KovaletsIvan@users.noreply.github.com>
2021-09-29 20:48:10 +0300

    commit

---
+++ react-8-3/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Dimentions from "./Dimentions";
+
+
+const App = () => {
+
+  return (<Dimentions />)
+}
+
+
+export default App
---
+++ react-8-3/src/Dimentions.jsx
@@ -0,0 +1,42 @@
+
+import React from 'react';
+
+class Dimentions extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      width: innerWidth,
+      height: innerHeight
+    }
+
+  }
+
+  componentDidMount() {
+    this.onResize = (e) => {
+      const { innerWidth, innerHeight } = e.target
+      this.setState({
+        width: innerWidth,
+        height: innerHeight
+      })
+    }
+   window.addEventListener('resize', this.onResize)
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.onResize)
+  }
+
+  render() {
+    document.title = `${this.state.width} x ${this.state.height}`
+    return (
+      <div className="dimensions">{`${this.state.width}px - ${this.state.height}px`}</div>
+    )
+  }
+}
+
+
+
+export default Dimentions
+
+
+
