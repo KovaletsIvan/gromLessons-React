861051aafc13faf1332465497ba9a243d629fab6
Ivan Kovalets <55809354+KovaletsIvan@users.noreply.github.com>
2021-10-01 22:49:48 +0300

    commit

---
+++ react-9-1/src/Search.jsx
@@ -0,0 +1,43 @@
+
+import React from 'react';
+
+class Search extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      text: '',
+    }
+
+  }
+
+  componentDidMount() {
+    this.clickHendle
+  }
+
+  clickHendle = (e) => {
+    e.preventDefault()
+    alert(`Search text: ${this.state.text}`)
+  }
+
+  listenInpat=(e)=>{
+    this.setState({
+     text: e.target.value 
+    })
+  }
+  render() {
+
+    return (
+      <form className="search">
+        <input type="text" className="search__input" onChange={this.listenInpat}/>
+        <button className="search__button" onClick={this.clickHendle}>Search</button>
+      </form>
+    )
+  }
+}
+
+
+
+export default Search
+
+
+
