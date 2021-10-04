ef99eb0479e178d14f3addef1d869b99e92a1694
Ivan Kovalets <55809354+KovaletsIvan@users.noreply.github.com>
2021-10-04 21:34:48 +0300

    commit

---
+++ react-9-2/src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import UserForm from "./UserForm";
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  createUser = (e,obj) => {
+    e.preventDefault()
+    console.log(obj)
+  }
+
+  render() {
+    return (<UserForm createUser={this.createUser} />)
+  }
+
+}
+
+
+export default App
