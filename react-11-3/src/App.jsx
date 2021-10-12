10dafa28f773eb21ef128d2f42198f77eeea158e
Ivan Kovalets <55809354+KovaletsIvan@users.noreply.github.com>
2021-10-12 23:52:41 +0300

    commit

--- react-11-3/src/App.jsx
+++ react-11-3/src/App.jsx
@@ -18,17 +18,31 @@ class App extends React.Component {
   }
 
   render() {
-    const content = this.state.isVisible ?
+    const isVisible = this.state.isVisible
+
+    const visible = isVisible ?
+      <i className="fas fa-chevron-up"></i>
+      : <i className="fas fa-chevron-down"></i>
+
+
+
+    const text = isVisible ?
       <p>
         Hooks are a new addition in React 16.8. They let you use state and other React features
         without writing a class.
       </p>
       : null
 
+    const content = isVisible ?
+      <div className="expand__content">
+        {text}
+      </div> :
+      null
+
     return (
       <div className="app">
         <Expand
-          isVisible={this.state.isVisible}
+          isVisible={visible}
           title="Some title"
           onClick={this.hendelClick}
         >
--- react-11-3/src/Expand.jsx
+++ react-11-3/src/Expand.jsx
@@ -2,26 +2,19 @@ import React from "react";
 
 
 const Expand = ({ children, isVisible, title, onClick }) => {
-  const visibility = isVisible ?
-    <i className="fas fa-chevron-up"></i>
-    : <i className="fas fa-chevron-down"></i>
 
-  const content = isVisible ?
-    <div className="expand__content">
-      {children}
-    </div> :
-    null
+
+
   return (
-
 
     <div className="expand border">
       <div className="expand__header">
         <span className="expand__title">{title}</span>
         <button className="expand__toggle-btn" onClick={onClick}>
-          {visibility}
+          {isVisible}
         </button>
       </div>
-      {content}
+      {children}
     </div>
 
 
