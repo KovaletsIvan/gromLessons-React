0e600a525baee8994ac3219d5eac3c83fcb4b1fc
Ivan Kovalets <55809354+KovaletsIvan@users.noreply.github.com>
2021-10-12 22:12:44 +0300

    commit

--- react-11-3/src/App.jsx
+++ react-11-3/src/App.jsx
@@ -19,31 +19,18 @@ class App extends React.Component {
 
 
   render() {
-    const visibility = this.state.isVisible ?
-      <i className="fas fa-chevron-up"></i>
-      : <i className="fas fa-chevron-down"></i>
     const context = this.state.isVisible ?
-      <div className="expand__content">
-        <p>
-          Hooks are a new addition in React 16.8. They let you use state and other React features
-          without writing a class.
-        </p>
+      <div className="expand__content"> <p>
+        Hooks are a new addition in React 16.8. They let you use state and other React features
+        without writing a class.
+      </p>
       </div>
       : null
 
-    const btn = <button className="expand__toggle-btn" onClick={this.hendelClick}>
-      {visibility}
-    </button>
     return (
-      <div className="app">
-        <div className="expand border">
-          <Expand title='Some title'>
-            {btn}
-          </Expand>
-
-          {context}
-        </div>
-      </div>
+      <Expand isVisible={this.state.isVisible} title="Some title" onClick={this.hendelClick}>
+        {context}
+      </Expand>
     )
   }
 
--- react-11-3/src/Expand.jsx
+++ react-11-3/src/Expand.jsx
@@ -1,18 +1,27 @@
 import React from "react";
 
-const Expand = ({ children, title }) => {
 
+const Expand = ({ children, isVisible, title, onClick }) => {
+  const visibility = isVisible ? <i className="fas fa-chevron-up"></i> : <i className="fas fa-chevron-down"></i>
   return (
+    <div className="app">
 
+      <div className="expand border">
+        <div className="expand__header">
+          <span className="expand__title">{title}</span>
+          <button className="expand__toggle-btn" onClick={onClick}>
+            {visibility}
+          </button>
+        </div>
 
-    <div className="expand__header">
-      <span className="expand__title">{title}</span>
-      {children}
+        {children}
+
+      </div>
     </div>
-
 
   )
 }
+
 
 
 
