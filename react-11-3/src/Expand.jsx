5bdccb44287d47c3ff76ef22a16469fb5c8a9257
Ivan Kovalets <55809354+KovaletsIvan@users.noreply.github.com>
2021-10-12 22:09:18 +0300

    commit

--- react-11-3/src/Expand.jsx
+++ react-11-3/src/Expand.jsx
@@ -1,37 +1,19 @@
 import React from "react";
-import PropTypes from "prop-types"
 
-const Expand = ({ children, isVisible, title, onClick }) => {
-  const visibility = isVisible ? <i className="fas fa-chevron-up"></i> : <i className="fas fa-chevron-down"></i>
+const Expand = ({ children, title }) => {
+
   return (
-    <div className="app">
 
-      <div className="expand border">
-        <div className="expand__header">
-          <span className="expand__title">{title}</span>
-          <button className="expand__toggle-btn" onClick={onClick}>
-            {visibility}
-          </button>
-        </div>
 
-        {children}
-
-      </div>
+    <div className="expand__header">
+      <span className="expand__title">{title}</span>
+      {children}
     </div>
+
 
   )
 }
 
-Expand.PropTypes = {
-  children: PropTypes.element.isRequired,
-  isVisible: PropTypes.bool,
-  onClick: PropTypes.func.isRequired
-}
-
-Expand.defaultProp = {
-  title: 'Some other text!',
-  isVisible: false
-}
 
 
 export default Expand
