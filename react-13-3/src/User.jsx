09d930ef7898c1e5a287c1f81ff27bfde4a4a61f
Ivan Kovalets <55809354+KovaletsIvan@users.noreply.github.com>
2021-10-21 17:11:08 +0300

    commit

--- react-13-3/src/User.jsx
+++ react-13-3/src/User.jsx
@@ -1,42 +1,43 @@
-import React from "react";
+import React from 'react';
 
 class User extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      data: '',
-      userid: this.props.match.params.userId
-    }
+      data: {},
+      userid: this.props.match.params.userId,
+    };
   }
   componentDidMount() {
     this.getData(this.props.match.params.userId);
   }
-
+  componentDidUpdate(privProps) {
+    if (this.props.match.params.userId !== privProps.match.params.userId) {
+      this.getData(this.props.match.params.userId);
+    }
+  }
   getData = (id) => {
     fetch(`https://api.github.com/users/${id}`)
-      .then(respose => respose.json())
-      .then(result => {
+      .then((respose) => respose.json())
+      .then((result) => {
         this.setState({
           data: result
-        })
-      })
-  }
+        });
+      });
+  };
   render() {
-    const { name, location, avatar_url } = this.state.data
-    return <div div className="user" >
-      <img alt="User Avatar"
-        src={avatar_url}
-        className="user__avatar" />
-      <div className="user__info">
-        <span className="user__name">{name}</span>
-        <span className="user__location">{location}</span>
+
+    const { name, location, avatar_url } = this.state.data;
+    return (
+      <div div className="user">
+        <img alt="User Avatar" src={avatar_url} className="user__avatar" />
+        <div className="user__info">
+          <span className="user__name">{name}</span>
+          <span className="user__location">{location}</span>
+        </div>
       </div>
-    </div >
-
+    );
   }
-
 }
 
-
-
-export default User
+export default User;
