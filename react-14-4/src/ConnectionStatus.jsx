eec5cf1cd99b6b3dfdb39a36b95ea8e20d9619ab
Ivan Kovalets <55809354+KovaletsIvan@users.noreply.github.com>
2021-10-25 20:48:41 +0300

    commit

--- react-14-4/src/ConnectionStatus.jsx
+++ react-14-4/src/ConnectionStatus.jsx
@@ -1,16 +1,16 @@
 import React, { useEffect, useState } from "react";
 
 const ConnectionStatus = () => {
-  const [status, setStatus] = useState('Online')
+  const [status, setStatus] = useState('online')
 
   useEffect(() => {
-    window.addEventListener('offline', () => setStatus('Offline'))
-    window.addEventListener('online', () => setStatus('Online'))
+    window.addEventListener('offline', () => setStatus('offline'))
+    window.addEventListener('online', () => setStatus('online'))
     return () => window.removeEventListener()
 
   }, [])
 
-  const statusClass = status === 'Online' ? "status " : "status status_offline"
+  const statusClass = status === 'online' ? "status " : "status status_offline"
 
   return (
     <div className={statusClass}>{status}</div>
