7e07373171af32e11081fffb18e0c0563f5a8d7b
Ivan Kovalets <55809354+KovaletsIvan@users.noreply.github.com>
2021-10-25 22:41:37 +0300

    commit

---
+++ react-14-5/src/Clock.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import moment from "moment";
+
+const timeFormat = (date) => moment(date).format('LTS')
+
+const getTimeWithOffset = offset => {
+  const currentTime = new Date();
+  const utcOffset = currentTime.getTimezoneOffset() / 60;
+  return new Date(currentTime.setHours(currentTime.getHours() + offset + utcOffset));
+}
+
+const Clock = ({ location, offset }) => {
+  const [time, setTime] = useState()
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(timeFormat(new Date(getTimeWithOffset(offset))))
+      return () => clearInterval(interval)
+    }, 1000)
+  }, [])
+
+
+
+  return (
+    <div className="clock">
+      <div className="clock__location">
+        {location}
+      </div>
+      <div className="clock__time">
+        {time}
+      </div>
+    </div>
+  )
+}
+
+export default Clock
